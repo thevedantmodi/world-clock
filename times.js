fbb64c453226ca4cb5dfb977d4f2d4b111dd2e1e
Vedant Modi <59243871+legoironman1234@users.noreply.github.com>
2023-06-21 08:36:31 -0500

    Add files via upload

---
+++ times.js
@@ -0,0 +1,28 @@
+const locations = document.querySelectorAll("section.times div")
+
+
+const updateTimes = function () {
+    locations.forEach(port => {
+        const output = port.querySelector("output")
+        const tz = port.getAttribute("tz")
+
+        const now = luxon.DateTime.now().setZone(tz)
+        
+        const hour = parseInt(now.toFormat("H"))
+
+        if (hour >= 8 && hour < 23) {
+            port.classList.add("awake")
+        }
+    
+
+
+        output.innerHTML = now.toFormat("HH:mm:ss")
+    })
+}
+
+
+updateTimes()
+
+setInterval(function () {
+    updateTimes()
+}, 1000)
