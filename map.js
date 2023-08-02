c354ed2da8f8b854c8ef436e68a919aa9f09acff
Vedant Modi <vedantmodi@gmail.com>
2023-08-02 10:16:37 -0500

    Set center on 0,0 and no repeating maps

--- map.js
+++ map.js
@@ -1,2 +1,7 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([0,0], 170);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    noWrap: true
+}).addTo(map);
