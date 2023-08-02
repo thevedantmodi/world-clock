262fad7853cee204846e439744e840ddd161cae0
Vedant Modi <vedantmodi@gmail.com>
2023-08-02 15:58:42 -0500

    IATAairports.json added

--- markers.js
+++ markers.js
@@ -1,4 +1,9 @@
 import { location_names } from "./locations.js";
+import { airports } from "./airports.js";
+
+location_names.forEach(name => {
+    console.log(airports.get(name))
+})
 
 console.log(location_names)
 
