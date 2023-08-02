b55e9905653fbcce3cd798085a56048f38c8d1a3
Vedant Modi <vedantmodi@gmail.com>
2023-08-02 16:56:15 -0500

    Proper import of IATAairports.json

--- markers.js
+++ markers.js
@@ -1,9 +1,34 @@
 import { location_names } from "./locations.js";
-import { airports } from "./airports.js";
+// import { airports } from "./airports.js";
 
-location_names.forEach(name => {
-    console.log(airports.get(name))
-})
+const URL = 'IATAairports.json';
+let airports = new Map()
 
-console.log(location_names)
+fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {            
+            airports.set(item.iata, item);
+        });
+        
+        makeMarkers(airports)
+    })
+    .catch(error => {
+        console.error('Error fetching JSON data:', error);
+    });
+
+
+function makeMarkers (airports) {
+    console.log(airports.get("LAX"))
+}
+
+// // location_names.forEach(name => {
+//     console.log(airports.get('BOS'))
+// // })
+
+// airports.forEach(port => {
+//     console.log(port)
+// })
+
+// console.log(location_names)
 
