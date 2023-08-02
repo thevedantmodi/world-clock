36458499ccce8ed2d71a333cefaf7bce81609bde
Vedant Modi <vedantmodi@gmail.com>
2023-08-02 11:21:53 -0500

    Map default view

--- map.js
+++ map.js
@@ -1,7 +1,15 @@
-var map = L.map('map').setView([0,0], 170);
+var map = L.map('map', {
+    minZoom: 1.5,
+    maxZoom: 10,
+});
+
+const OSM_attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    noWrap: true
+    attribution: OSM_attribution,
+    noWrap: false
 }).addTo(map);
+
+
+
+map.setView([27,0],0);
