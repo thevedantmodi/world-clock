75c9f25551e9de76ffc1cf8c275f34f93f32d758
Vedant Modi <vedantmodi@gmail.com>
2023-08-04 16:32:11 -0500

    Add marker plot on query

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # world-clock
 
-
 ## Sources
 https://dev.to/deepakdevanand/leaflet-map-dark-theme-5ej0
+
+https://www.youtube.com/shorts/UqBZN1A437I
+
+https://developers.arcgis.com/documentation/mapping-apis-and-services/maps/services/basemap-layer-service/
+
 
--- map.js
+++ map.js
@@ -15,18 +15,37 @@ import { location_names } from "./locations.js";
 // ]
 import * as airports from "./airports.js";
 
+// TODO: Change to non temp key
+const apiKey = "AAPKd5f4ec47348045939bbc7b3325751f2bAJtvnrN1ekzxq--Il1sHOL2c-IrCJ-3c2IPRMUhuh5wa4KxweuzwY5hkI2GyqBDR";
+const basemapEnum = "ArcGIS:DarkGray";
+
 // minZoom: 1.5 maxZoom: 10
 // Maybe lock movement, you don't really need to move around?
-const map = {};
-
-const OSM_attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-const Vedant_attribution = '&copy; <a href="http://www.vedantmodi.com">Vedant Modi</a> '
-
-// no wrap
 
 
 // Point a little up from the center to show more of land-dense N Hemisphere
 // center is lat, lon = 27, 0
+
+
+const map = new maplibregl.Map({
+    container: "map", // the id of the div element
+    style: `https://basemaps-api.arcgis.com/arcgis/rest/services/styles/${basemapEnum}?type=style&token=${apiKey}`,
+    zoom: 1.2,
+    center: [0, 20], // starting location [longitude, latitude]
+    attributionControl: false,
+    noWrap: false
+    // TODO: nowrap
+  })
+
+const OSM_attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+const Vedant_attribution = '&copy; <a href="http://www.vedantmodi.com">Vedant Modi</a> '
+
+map.addControl(
+    new maplibregl.AttributionControl({
+      compact: true,// reduces the copyright attributions view
+      customAttribution: Vedant_attribution + OSM_attribution
+    })
+  );
 
 let markers = []
 let tooltips = []
@@ -37,19 +56,32 @@ export function makeMarkers () {
     console.log(`all done!`)
 }
 
-setInterval(function() {
-    displayTimes()
-}, 1000)
+// setInterval(function() {
+//     displayTimes()
+// }, 1000)
 
 function plotMarkers () {
     const time_blocks = document.querySelectorAll(".times div")
     for (let i = 0; i < time_blocks.length; i++) {
-        const lat = time_blocks[i].lat
         const lon = time_blocks[i].lon
+        const lat = time_blocks[i].lat
+
+        let icon = new maplibregl.Marker({
+            element: plotPoint(),
+          }).setLngLat([lon, lat]) // Replace with the coordinates of the point you want to add
+            .addTo(map);
+          
+          
         // black dot 7px x 7px
         // at [lon, lat]
     }
 }
+
+function plotPoint() {
+    var element = document.createElement('div');
+    element.className = 'map-point';
+    return element;
+  }
 
 function plotTooltips () {
     const time_blocks = document.querySelectorAll(".times div")
