16605678b7b001456ae62fcae5506f0b0cb0e099
Vedant Modi <vedantmodi@gmail.com>
2023-08-09 01:13:12 -0500

    Remove toggle for now - too buggy

---
+++ city-toggle.js
@@ -0,0 +1,18 @@
+const city_toggle = document.querySelector(".btn-container input")
+
+city_toggle.addEventListener('change', function() {
+    if (this.checked) {
+      console.log("code")
+    } else {
+        console.log("city")
+      
+    }
+  });
+
+function translatetoCity() {
+
+}
+
+function translatetoCode() {
+
+}
